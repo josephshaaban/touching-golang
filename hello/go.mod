0ef9849626cb2d62f64b5acf88453b9fae472429
Youssef Shaaban <joseph.sha3ban@gmail.com>
2024-01-24 18:46:59 +0400

    Get started with GoLang

---
+++ hello/go.mod
@@ -0,0 +1,7 @@
+module yosh/hello
+
+go 1.21.3
+
+require yosh/greetings v0.0.0-00010101000000-000000000000
+
+replace yosh/greetings => ../greetings
---
+++ hello/hello.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	
+	"yosh/greetings"
+)
+
+func main(){
+	// Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	
+	// Request a greeting message
+	msg, err := greetings.Hello("")
+	// If an error was returned, print it to the console and
+    // exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	fmt.Println(msg)
+}

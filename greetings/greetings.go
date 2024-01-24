c0f91d3748ec92668583b91d74ef1c5be3800905
Youssef Shaaban <joseph.sha3ban@gmail.com>
2024-01-24 19:01:56 +0400

    random greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,19 +1,35 @@
 package greetings
 
 import (
-	"fmt"
 	"errors"
+	"fmt"
+	"math/rand"
 )
 
 // Hello returns greeting for the named person
-func Hello(name string) (string, error){
+func Hello(name string) (string, error) {
 	// if no name was given, return an error with a message
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	
-    // Return a greeting that embeds the name in a message.
+
+	// Return a greeting that embeds the name in a message.
 	// var message string
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	// A slice of message formats
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// randomFormat returns one of a set of greeting messages. The returned
+	// message is selected at random.
+	return formats[rand.Intn(len(formats))]
 }
--- hello/hello.go
+++ hello/hello.go
@@ -3,24 +3,24 @@ package main
 import (
 	"fmt"
 	"log"
-	
+
 	"yosh/greetings"
 )
 
-func main(){
+func main() {
 	// Set properties of the predefined Logger, including
-    // the log entry prefix and a flag to disable printing
-    // the time, source file, and line number.
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-	
+
 	// Request a greeting message
-	msg, err := greetings.Hello("")
+	msg, err := greetings.Hello("Yosh")
 	// If an error was returned, print it to the console and
-    // exit the program.
+	// exit the program.
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	fmt.Println(msg)
 }
